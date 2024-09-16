52fa6dfcbe0de97636ae25d041b8efca76396b62
Hajira-Mirza <hajiramrz@gmail.com>
2024-09-17 03:10:35 +0500

    Implement auth, appwwrite, and sentry

--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -13,7 +13,7 @@ const BankCard = ({
       <Link href="/" className="bank-card">
         <div className="bank-card_content">
           <h1 className="text-16 font-semibold text-white">
-            {account.name || userName}
+            {userName}
           </h1>
           <p className="font-ibm-plex-serif font-black text-white">
             {formatAmount(account.currentBalance)}
